781e1d0179afec56665eb41489bdac19e7dc3100
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2014-06-10 16:40:47 -0700

    Working demo page

---
+++ js/demo.application.js
@@ -0,0 +1,23 @@
+(function() {
+  $(function () {
+    $('.proceed').on('click', function(e) {
+      $('#checkout').show();
+      SC.init({
+        showImmediately: true,
+        autoFill: true
+      }, function(data) {
+        console.log(data);
+      });
+    });
+
+    $('.product').on('click', '.product-add', function(e) {
+      var $cart = $('.cart');
+      if ($cart.hasClass('close')) {
+        $cart.animate({
+          bottom: 0
+        }, 500);
+      }
+      $('<li class="cart-item">').text($('.product-info h4', e.delegateTarget).text()).appendTo($('.cart-items'));
+    });
+  });
+})();
