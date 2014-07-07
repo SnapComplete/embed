4ed997704da39e6e8b2e65ae9ee7786cb30bc241
Choon Hong Peck <chpeck@gmail.com>
2014-07-07 14:31:50 -0700

    Convert to using ratchet

--- js/demo.application.js
+++ js/demo.application.js
@@ -1,8 +1,13 @@
 (function() {
   $(function () {
+    $('.products').on('click', '.product-add', function() {
+      if(!$('.checkout').is(':visible')) {
+        $('.checkout').slideDown('slow');
+      }
+    });
 
-    $('.proceed').on('click', function(e) {
-      $('#checkout').show();
+    $('.product-add').first().click();
+    window.addEventListener('push', function() {
       SC.init({
         showImmediately: true,
         autoFill: true
@@ -11,20 +16,5 @@
       });
     });
 
-    $('.product').on('click', '.product-add', function(e) {
-      var $cart = $('.cart');
-      if ($cart.hasClass('close')) {
-        $cart.animate({
-          bottom: 0
-        }, 500);
-      }
-      $('<li class="cart-item">').text($('.product-info h4', e.delegateTarget).text()).appendTo($('.cart-items'));
-    });
-
-    $('button.submit').on('click', function(e) {
-      $('#checkout').hide();
-    });
-
-    $('a.product-add').first().click();
   });
 })();
